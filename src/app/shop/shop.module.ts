a1d65ba64d7ff2afcaa7f65b1bf53ca7748bfb56
batuhan <batuhanbskr@gmail.com>
2023-11-28 20:26:28 +0300

    Url Routing - CartDetail and checkout component

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NavbarComponent } from './navbar/navbar.component';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
     imports:  [ModelModule, BrowserModule, FormsModule],
@@ -12,9 +14,11 @@ import { CartSummaryComponent } from './cart-summary/cart-summary.component';
     declarations: [
       ShopComponent,
       NavbarComponent,
-      CartSummaryComponent
+      CartSummaryComponent,
+      CartDetailComponent,
+      CheckoutComponent
     ],
-    exports: [ShopComponent]
+    exports: [ShopComponent, CartDetailComponent, CheckoutComponent]
 })
 
 export class ShopModule {}
